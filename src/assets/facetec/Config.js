a27a0315dc69b89c72546324ef7cd6e6b93eced5
Maria Isa <maria.lamus@ciberdix.com>
2021-10-05 16:19:37 -0500

    Prueba de configuración

--- src/assets/facetec/Config.js
+++ src/assets/facetec/Config.js
@@ -3,7 +3,7 @@ export var Config = (function () {
     // REQUIRED
     // Available at https://dev.facetec.com/#/account
     // NOTE: This field is auto-populated by the FaceTec SDK Configuration Wizard.
-    var DeviceKeyIdentifier = 'd2mqkSIyJEtasi8svxYSCH6qsLXef3yx';
+    var DeviceKeyIdentifier = '';
 
     // -------------------------------------
     // REQUIRED
@@ -11,7 +11,7 @@ export var Config = (function () {
     // In Production, you likely will handle network requests elsewhere and without the use of this variable.
     // See https://dev.facetec.com/#/security-best-practices?link=facetec-server-rest-endpoint-security for more information.
     // NOTE: This field is auto-populated by the FaceTec SDK Configuration Wizard.
-    var BaseURL = 'http://192.168.8.18:8080';
+    var BaseURL = '';
 
     // -------------------------------------
     // REQUIRED
